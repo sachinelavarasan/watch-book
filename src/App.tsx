67262d573cc0174f0676455c8c18f82b483fbbc8
sachinelavarasan <sachinelavarasan321@gmail.com>
2023-02-05 22:52:05 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import { FaBeer } from 'react-icons/fa';
+import './App.css';
+import { RootState, useAppDispatch, useAppSelector } from './redux/store';
+import { Button } from '@mui/material';
+import { increment } from './redux/slices/counterSlice';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
+function App() {
+  const { value } = useAppSelector((state: RootState) => state.counter);
+  const dispatch = useAppDispatch();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <FaBeer /> Edit <code>src/App.tsx</code> and save to reload.{value}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+        <Button variant="contained" onClick={() => dispatch(increment())}>
+          Contained
+        </Button>
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/instance.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:7000',
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('jwtToken');
+    // const userId = localStorage.getItem('userId');
+    const headers = { ...config.headers };
+    headers.set('Authorization', `Bearer ${token}`);
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
+
+export default instance;
