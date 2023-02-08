a0e2f313946edfff4d4c09726658c1db623ab409
janani811 <jananigopalsanthi@gmail.com>
2023-02-08 16:35:12 +0530

    navbar implementation

---
+++ src/common/Navbar/components/Icon/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import { IconContext } from 'react-icons';
+import { NavLink } from 'react-router-dom';
+import { IconContainer } from './elements';
+
+function Icon({ route, iconComponent, toolTipTitle }: any) {
+  return (
+    <IconContainer>
+      <NavLink to={route} className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+        <Tooltip title={toolTipTitle} arrow placement="right">
+          <div className="icon-container">
+            <IconContext.Provider
+              value={{
+                size: '1.7rem',
+                className: 'activeState',
+              }}>
+              {iconComponent}
+            </IconContext.Provider>
+          </div>
+        </Tooltip>
+      </NavLink>
+    </IconContainer>
+  );
+}
+
+export default Icon;
