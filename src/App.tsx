18493e9f64400cca2a909207328f7a6996e6e3fe
janani811 <jananigopalsanthi@gmail.com>
2023-02-08 15:30:02 +0530

    navbar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import { FaBeer } from 'react-icons/fa';
+import logo from './logo.svg';
+import { FaBeer } from 'react-icons/fa';
 import './App.css';
-// import { RootState, useAppDispatch, useAppSelector } from './redux/store';
-// import { Button } from '@mui/material';
-// import { increment } from './redux/slices/counterSlice';
+import { RootState, useAppDispatch, useAppSelector } from './redux/store';
+import { Button } from '@mui/material';
+import { increment } from './redux/slices/counterSlice';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Navbar from './common/Navbar';
 
 function App() {
-  // const { value } = useAppSelector((state: RootState) => state.counter);
-  // const dispatch = useAppDispatch();
+  const { value } = useAppSelector((state: RootState) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <FaBeer /> Edit <code>src/App.tsx</code> and save to reload.{value}
@@ -30,18 +29,12 @@ function App() {
         <Button variant="contained" onClick={() => dispatch(increment())}>
           Contained
         </Button>
-      </header> */}
-
+      </header>
       <BrowserRouter>
-        <div className="app-main-container">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="dashboard" element={<SignUp />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<SignUp />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-// import { ThemeProvider } from '@emotion/react';
-// import theme from './utils/theme';
+import { ThemeProvider } from '@emotion/react';
+import theme from './utils/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-    {/* </ThemeProvider> */}
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
