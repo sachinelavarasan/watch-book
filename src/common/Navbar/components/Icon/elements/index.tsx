70e35a9ce37b2f9df5d619bce026b7bad5091fad
janani811 <jananigopalsanthi@gmail.com>
2023-02-08 14:51:32 +0530

    navbar implementation

---
+++ src/common/Navbar/components/Icon/elements/index.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const IconContainer = styled.div`
+  .active {
+    .icon-container {
+      background-color: white;
+      border-radius: 0.25rem;
+      opacity: 0.8;
+    }
+    .activeState {
+      color: black;
+    }
+  }
+  .inactive {
+    .icon-container {
+      background-color: transparent;
+    }
+    .activeState {
+      color: white;
+    }
+  }
+  .icon-container {
+    padding: 0.325rem;
+  }
+`;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Login() {
+  console.log('hai');
   return <div>Login</div>;
 }
 
