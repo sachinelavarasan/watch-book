7009f596800e699ca9241a27e4561ab225ec358f
sachinelavarasan <sachinelavarasan321@gmail.com>
2024-01-19 01:30:53 +0530

    env

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,6 +1,6 @@
 import axios from '../instance';
 
-export const logIn = (data: any) => axios.post('/auth/signin', data);
+export const logIn = (data: any) => axios.post('/auth/login', data);
 export const register = (data: any) => axios.post('/auth/signup', data);
 export const fetchProfile = () => axios.get('/auth/getprofile');
-export const editProfile = (data: any) => axios.post('/auth/editprofile', data);
+export const editProfile = (data: any) => axios.put('/auth/edit-profile', data);
